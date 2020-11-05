3ac96636bcf6292ffd166b0c869382a939e0410d
esta-rossa <rajiabdou9@gmail.com>
2020-11-05 10:43:41 +0100

    clearing repo

--- minishell/includes/env.h
+++
@@ -1,7 +0,0 @@
-#ifndef FT_ENV_H
-# define FT_ENV_H
-#include "minishell.h"
-char	*get_var_key(char *var);
-char	*get_var_value(char *var);
-char	**list_to_tab(t_env *env);
-#endif
--- minishell/includes/to_delete.h
+++
@@ -1,6 +0,0 @@
-#ifndef FT_TO_DELETE_H
-# define FT_TO_DELETE_H
-#include "minishell.h"
-void	init_var(t_env **env, char **list_env);
-void	init_cmd(t_cmd **cmd, char *line);
-#endif
